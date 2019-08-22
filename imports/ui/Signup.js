9b1dff3e29318ae77f985289865cc190b824db7b
NandeDatatimDev <nande@datatim.co.za>
2019-08-22 14:48:51 +0200

    56.(A) Component State and Signup Form (created an increment & decrement button for prac)

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,11 +2,28 @@ import React from 'react';
 import {Link} from "react-router";
 
 export default class Signup extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+          count:0
+        };
+    }
+    increment(){
+        this.setState({
+            count: this.state.count+1
+        })
+    }
+    decrement(){
+        this.setState({
+            count: this.state.count-1
+        })
+    }
     render() {
         return <div>
-            <h1>Signup Here!</h1>
-            Signup form here
-            <Link to='/'>Have an account! </Link>
+            <h1>Join Short Lnk</h1>
+            <p>{this.state.count}</p>
+            <p><button onClick={this.increment.bind(this)}>+1</button><button onClick={this.decrement.bind(this)}>-1</button></p>
+            <Link to='/'>Already have an account! </Link>
         </div>
     }
 }
