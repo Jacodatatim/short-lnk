15117456a102a99e936ed9738d0c580d160be603
NandeDatatimDev <nande@datatim.co.za>
2019-08-22 12:40:18 +0200

    start app v.52

--- client/main.js
+++ client/main.js
@@ -1,6 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Signup from "../imports/ui/Signup";
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  // code to run on client at startup
+    ReactDOM.render(<Signup/>,document.getElementById('app'));
 });
 
